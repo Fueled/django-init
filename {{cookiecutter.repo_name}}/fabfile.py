5f642edf22cf012147069c5466c5351bf3666683
Saurabh Kumar <me@saurabh-kumar.com>
2014-05-25 02:03:18 +0530

    feat(command): add 'fab shell' command

--- {{cookiecutter.repo_name}}/fabfile.py
+++ {{cookiecutter.repo_name}}/fabfile.py
@@ -20,8 +20,20 @@ def init(vagrant=True):
 
 
 def configure():
-    pass
+    local('npm install')
 
 
 def serve_doc(address='127.0.0.1', port='8001'):
     local('mkdocs serve --dev-addr=%s:%s' % (address, port))
+
+
+def manage(cmd):
+    local('python {{ coookiecutter.repo_name}}/manage.py {}'.format(cmd))
+
+
+def shell():
+    manage('shell_plus')
+
+
+def serve():
+    manage('runserver')
