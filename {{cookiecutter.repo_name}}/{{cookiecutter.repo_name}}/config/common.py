839d5d9d2603be26051341ec48caf9dc06c2eb99
Saurabh Kumar <me@saurabh-kumar.com>
2014-06-13 23:46:30 +0530

    update documentation

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/common.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/common.py
@@ -98,15 +98,28 @@ class Common(Configuration):
     DATABASES = values.DatabaseURLValue('postgres://{{cookiecutter.repo_name}}@localhost/{{cookiecutter.repo_name}}')
     # END DATABASE CONFIGURATION
 
-    # GENERAL CONFIGURATION
-    # Do not change them, unless you know what you are doing.
-    TIME_ZONE = '{{ cookiecutter.timezone }}'
-    LANGUAGE_CODE = 'en-us'
     SITE_ID = 1
+
+    # Local time zone for this installation. Choices can be found here:
+    # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+    # although not all choices may be available on all operating systems.
+    # In a Windows environment this must be set to your system time zone.
+    TIME_ZONE = '{{ cookiecutter.timezone }}'
+
+    # Language code for this installation. All choices can be found here:
+    # http://www.i18nguy.com/unicode/language-identifiers.html
+    LANGUAGE_CODE = 'en-us'
+
+    # If you set this to False, Django will make some optimizations so as not
+    # to load the internationalization machinery.
     USE_I18N = True
+
+    # If you set this to False, Django will not format dates, numbers and
+    # calendars according to the current locale.
     USE_L10N = True
+
+    # If you set this to False, Django will not use timezone-aware datetimes.
     USE_TZ = True
-    # END GENERAL CONFIGURATION
 
     # TEMPLATE CONFIGURATION
     TEMPLATE_CONTEXT_PROCESSORS = (
@@ -126,6 +139,7 @@ class Common(Configuration):
         join(BASE_DIR, 'templates'),
     )
 
+    # List of callables that know how to import templates from various sources.
     TEMPLATE_LOADERS = (
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
@@ -143,7 +157,8 @@ class Common(Configuration):
         join(BASE_DIR, 'static'),
     )
 
-    # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
+    # List of finder classes that know how to find static files in
+    # various locations.
     STATICFILES_FINDERS = (
         'django.contrib.staticfiles.finders.FileSystemFinder',
         'django.contrib.staticfiles.finders.AppDirectoriesFinder',
