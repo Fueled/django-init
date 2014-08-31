4328b3dca56ef2062ce0bfdadbc044b4072126ca
Saurabh Kumar <me@saurabh-kumar.com>
2014-08-31 11:11:22 +0530

    fix(config/celery): fix import issue for celery

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/__init__.py
@@ -1,7 +1,1 @@
 # -*- coding: utf-8 -*-
-''' Project Configs
-'''
-from __future__ import unicode_literals, absolute_import
-
-from .development import Development
-from .production import Production
