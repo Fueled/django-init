94bdee2b53d44ec974dd22d2f037b5f1350675e0
Saurabh Kumar <shk@fueled.co>
2014-10-01 22:04:16 +0530

    feat(fab): update the commands to work with virtualenv

--- {{cookiecutter.repo_name}}/fabfile.py
+++ {{cookiecutter.repo_name}}/fabfile.py
@@ -3,49 +3,42 @@
 
 See: http://www.fabfile.org/
 '''
-from os.path import join, abspath, dirname
-from fabric.api import local, env, lcd
+from __future__ import unicode_literals, absolute_import, with_statement
+import os
+from os.path import join, isdir
+from fabric.api import local, env, lcd, cd, prefix
+from contextlib import contextmanager as _contextmanager
 
-here = abspath(join(dirname(__file__)))
+here = os.getcwd()
 
-env.project = '{{ cookiecutter.repo_name }}'
-env.apps_dir = join(here, env.project)
+env.project_name = '{{ cookiecutter.repo_name }}'
+env.apps_dir = join(here, env.project_name)
 env.dotenv_path = join(env.apps_dir, '.env')
 env.requirements_file = join(here, 'configuration/pip/development.txt')
+env.shell = "/bin/bash -l -i -c"
 
 
-def init(vagrant=True):
+def init(vagrant=False):
     '''Prepare a local machine for development.'''
 
-    distro = local('uname -s', capture=True)
-
-    if distro == 'Darwin':  # OSX
-        env.installer = 'brew cask install'
-    elif distro == 'Linux':
-        env.installer = 'sudo apt-get install'
-    else:
-        print "Error: Your operating system (%s) is not supported yet" % distro
-    if vagrant:
-        local('%(installer)s virtualbox vagrant' % env)
-        local('vagrant plugin install vagrant-vbguest')
-    local('sudo pip install ansible')
+    install_deps()
     config('set', 'DJANGO_SECRET_KEY', '`openssl rand -base64 32`')
+    local('createdb %(project_name)s' % env)
+    config('set', 'DATABASE_URL', 'postgres://localhost/%(project_name)s' % env)
 
 
 def install_deps():
     '''Install project dependencies.'''
+    verify_virtualenv()
+    # activate virtualenv and install
+    with virtualenv():
+        local('pip install -r %s' % file)
     local('npm install')
-    local('pip install -r %(requirements_file)s' % env)
 
 
 def serve_doc(address='127.0.0.1', port='8001'):
     with lcd(here):
         local('mkdocs serve --dev-addr=%s:%s' % (address, port))
-
-
-def manage(cmd):
-    with lcd(here):
-        local('python {}/manage.py {}'.format(env.project, cmd))
 
 
 def shell():
@@ -55,6 +48,7 @@ def shell():
 def serve(host='127.0.0.1:8000'):
     '''Start an enhanced runserver'''
     install_deps()
+    migrate()
     manage('runserver_plus %s' % host)
 
 
@@ -63,8 +57,8 @@ def migrate():
     manage('migrate')
 
 
-def startapp(appname):
-    '''fab startapp <appname>
+def createapp(appname):
+    '''fab createapp <appname>
     '''
     path = join(env.apps_dir, appname)
     local('mkdir %s' % path)
@@ -77,10 +71,10 @@ def makemigrations(app):
 
 
 def config(action=None, key=None, value=None):
-    '''
-    Overwrites the .env file and set custom ENV variables
-    ref: https://github.com/theskumar/python-dotenv
-    example usage: fab config:set,[key],[value]
+    '''Manage project configuration via .env
+
+    see: https://github.com/theskumar/python-dotenv
+    e.g: fab config:set,[key],[value]
     '''
     local('touch %(dotenv_path)s' % env)
     command = 'dotenv'
@@ -89,3 +83,31 @@ def config(action=None, key=None, value=None):
     command += key if key else " "
     command += value if value else ""
     local(command)
+
+
+# Helpers
+# ------------------------------------------------------------------------------
+def manage(cmd, venv=True):
+    with virtualenv():
+        local('python {project}/manage.py %s' % (env.project_name, cmd))
+
+
+@_contextmanager
+def virtualenv():
+    '''Activates virtualenv context for other commands to run inside it
+    '''
+    with cd(here):
+        with prefix('source %(virtualenv_dir)s/bin/activate' % env):
+            yield
+
+
+def verify_virtualenv():
+    '''This modules check and install virtualenv if it not present.
+    It also creates local virtualenv directory if it's not present
+    '''
+    from distutils import spawn
+    if not spawn.find_executable('virtualenv'):
+        local('sudo pip install virtualenv')
+
+    if not isdir(env.virtualenv_dir):
+        local('virtualenv %(virtualenv_dir)s' % env)
