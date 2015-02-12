f24a46e617d5bb751c6cad78150677b0849ad7a3
Vikrant Pogula | Fueled <vikrant@fueled.co>
2015-02-12 05:57:25 +0530

    feat(base): add base timestamped UUID model

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/base/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django_extensions.db.fields import TimeStampedModel
+
+from .fields import AutoUUIDField
+
+
+class UUIDModel(models.Model):
+    '''
+    An abstract base class model that makes primary key `id` as UUID
+    instead of default auto incremented number.
+    '''
+    id = AutoUUIDField(primary_key=True, editable=False, null=False)
+
+    class Meta:
+        abstract = True
+
+    def __unicode__(self):
+        try:
+            return self.name
+        except AttributeError:
+            if self.id is not None:
+                return self.id
+            else:
+                return self.__class__.__name__+' object'
+
+
+class TimeStampedUUIDModel(TimeStampedModel, UUIDModel):
+    '''
+    An abstract base class model that provides self-updating
+    ``created`` and ``modified`` fields with UUID as primary_key field.
+    '''
+
+    class Meta:
+        abstract = True
