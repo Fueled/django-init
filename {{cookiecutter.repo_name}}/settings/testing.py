30d1880ab4cbd2c407c916f534f98a3723e792d1
Saurabh Kumar <shk@fueled.co>
2015-02-13 17:42:59 +0530

    chore(setup): hard code SECRET_KEY for test setting

--- {{cookiecutter.repo_name}}/settings/testing.py
+++ {{cookiecutter.repo_name}}/settings/testing.py
@@ -11,5 +11,7 @@ class Testing(Development):
 
     MEDIA_ROOT = "/tmp"
 
+    SECRET_KEY = 'top-scret!'
+
     EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
     INSTALLED_APPS = Development.INSTALLED_APPS + ("tests", )
