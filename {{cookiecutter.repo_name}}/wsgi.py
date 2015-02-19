d6531a9d49b76c93aca0ef3c71280ef99bd4e7df
Saurabh <saurabh@fueled.co>
2015-02-19 21:33:05 +0530

    fix(wsgi): make sure the get_wsgi_application is below env getter

--- {{cookiecutter.repo_name}}/wsgi.py
+++ {{cookiecutter.repo_name}}/wsgi.py
@@ -16,8 +16,6 @@ framework.
 """
 import os
 
-from configurations.wsgi import get_wsgi_application
-
 try:
     import dotenv
     ROOT_DIR = os.path.dirname(__file__)
@@ -35,6 +33,7 @@ os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
+from configurations.wsgi import get_wsgi_application  # noqa
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
