fe0b3e7bdceec9a5f1c2a9f890270203ed6b3d6b
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-05-02 15:13:12 +0530

    feat(django/core): replace core of UUIDModel to use native postgres UUID

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/base/models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/base/models.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
+# Standard Library
+import uuid
+
+# Third Party Stuff
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
-
-from .fields import AutoUUIDField
 
 
 class UUIDModel(models.Model):
@@ -12,19 +14,10 @@ class UUIDModel(models.Model):
     An abstract base class model that makes primary key `id` as UUID
     instead of default auto incremented number.
     '''
-    id = AutoUUIDField(primary_key=True, editable=False, null=False)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
 
     class Meta:
         abstract = True
-
-    def __unicode__(self):
-        try:
-            return self.name
-        except AttributeError:
-            if self.id is not None:
-                return self.id
-            else:
-                return self.__class__.__name__+' object'
 
 
 class TimeStampedUUIDModel(TimeStampedModel, UUIDModel):
