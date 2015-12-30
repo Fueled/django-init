9ad0e0c7bf56d95459183b7c33e5ace8565c2758
Saurabh Kumar <theskumar@users.noreply.github.com>
2015-12-30 15:58:58 +0530

    Remove dependency on django_extensions for TimeStampUUIDModel
    closes #122

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,7 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 - Default SMTP server to mailgun
 - Use `cache_db` as `SESSION_ENGINE` instead of simple `cache`
 - Use in-built `SecurityMiddleware` instead of django-secure
+- Remove dependency on `django_extensions` for `TimeStampUUIDModel`
 
 ## [1.0.0]
 ### Added
--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/base/models.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/base/models.py
@@ -1,41 +1,42 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
+from __future__ import absolute_import, unicode_literals
 
 # Standard Library
 import uuid
 
 # Third Party Stuff
 from django.db import models
-from django_extensions.db.models import TimeStampedModel
 from uuid_upload_path import upload_to
 from versatileimagefield.fields import PPOIField, VersatileImageField
 
 
 class UUIDModel(models.Model):
-    '''
+    """
     An abstract base class model that makes primary key `id` as UUID
     instead of default auto incremented number.
-    '''
+    """
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
 
     class Meta:
         abstract = True
 
 
-class TimeStampedUUIDModel(TimeStampedModel, UUIDModel):
-    '''
+class TimeStampedUUIDModel(UUIDModel):
+    """
     An abstract base class model that provides self-updating
     ``created`` and ``modified`` fields with UUID as primary_key field.
-    '''
+    """
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
 
     class Meta:
         abstract = True
 
 
 class ImageMixin(models.Model):
-    '''
+    """
     An abstract base class model that provides a VersatileImageField Image with POI
-    '''
+    """
 
     image = VersatileImageField(upload_to=upload_to, blank=True, null=True, ppoi_field='image_poi',
                                 verbose_name="image")
