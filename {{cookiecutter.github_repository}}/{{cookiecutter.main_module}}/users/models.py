be2d28d9a352522e58f53daf9a68813b6f0b9d8a
Saurabh Kumar <theskumar@users.noreply.github.com>
2016-02-24 15:19:41 +0530

    chore(users): remove redundant check for persence of email property

--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/users/models.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/users/models.py
@@ -18,8 +18,6 @@ class UserManager(BaseUserManager):
     def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
         """Creates and saves a User with the given email and password.
         """
-        if not email:
-            raise ValueError('The given email must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, is_staff=is_staff, is_active=True,
                           is_superuser=is_superuser, **extra_fields)
