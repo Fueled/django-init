af39d08b5d0a4c6d52e4e9dbe9b16c7ea71ea1ad
Saurabh Kumar <saurabh@saurabh-kumar.com>
2016-07-21 17:21:27 +0530

    style(base/views): update comment and indentation

--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/base/views.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/base/views.py
@@ -12,14 +12,12 @@ from django.views.defaults import server_error as default_server_error
 
 
 def server_error(request, *args, **kwargs):
-    """
-    JSON aware server 500 error response.
+    """JSON aware server 500 error response.
 
     As we don't want to return html response for a json request.
     """
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-
     if not settings.DEBUG and request.META.get('CONTENT_TYPE', None) == "application/json":
+        exc_type, exc_obj, exc_tb = sys.exc_info()
         response_dict = {
             'error_type': exc_type.__name__ if exc_type else 'ServerError',
             'errors': [{'message': 'Server application error', }]
