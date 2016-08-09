f29609944ebe14a7fb2b9cdaafb37113a63ebaab
Saurabh Kumar <thes.kumar@gmail.com>
2016-08-10 00:43:29 +0530

    Upgrade to flake8==2.6.*, handle F405 warnings (#175)

--- {{cookiecutter.github_repository}}/settings/testing.py
+++ {{cookiecutter.github_repository}}/settings/testing.py
@@ -1,18 +1,18 @@
 from __future__ import absolute_import, unicode_literals
 
-from .development import *  # noqa
+from .development import *  # noqa: F405
 
 MEDIA_ROOT = "/tmp"
 
 SECRET_KEY = 'top-scret!'
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
-INSTALLED_APPS += ("tests", )
+INSTALLED_APPS += ("tests", )  # noqa: F405
 {%- if cookiecutter.add_sass_with_django_compressor.lower() == 'y' %}
 
 # Do this here, so that test are run without `postcss` as a requirement.
 # Travis CI run with node 0.10.0 which is incompatible with the version
 # postcss required.
 # This causes the autoprefixer to not run on the complied css.
-COMPRESS_CSS_FILTERS.remove('django_compressor_autoprefixer.AutoprefixerFilter')
+COMPRESS_CSS_FILTERS.remove('django_compressor_autoprefixer.AutoprefixerFilter')  # noqa: F405
 {%- endif %}
