69ab59fa092981cc76ceb29dc9340894836f3044
Abhishek Kumar Singh <abhishek4bhopati@gmail.com>
2017-01-26 18:43:24 +0530

    Comment added for clarity.
    Signed-off-by: Abhishek Kumar Singh <abhishek4bhopati@gmail.com>

--- {{cookiecutter.github_repository}}/settings/testing.py
+++ {{cookiecutter.github_repository}}/settings/testing.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
+# Do this here, so that .env get loaded while running `py.test` from shell
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
+
 from .development import *  # noqa F405
 
 MEDIA_ROOT = "/tmp"
