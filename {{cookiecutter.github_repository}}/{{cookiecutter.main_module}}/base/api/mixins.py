e08508dbc154efd86e8874a32e6a970d9c3fb469
Mayank Jain <jainmickey93@gmail.com>
2017-08-03 17:50:33 +0530

    fix(base): update multi serializer mixin

--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/base/api/mixins.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/base/api/mixins.py
@@ -1,13 +1,16 @@
-class MultiSerializerViewSetMixin(object):
+from django.core.exceptions import ImproperlyConfigured
+
+
+class MultipleSerializerMixin(object):
     def get_serializer_class(self):
         """
-        Look for serializer class in self.serializer_action_classes, which
+        Look for serializer class in self.serializer_classes, which
         should be a dict mapping action name (key) to serializer class (value),
         i.e.:
 
-        class MyViewSet(MultiSerializerViewSetMixin, ViewSet):
+        class MyViewSet(MultipleSerializerMixin, ViewSet):
             serializer_class = MyDefaultSerializer
-            serializer_action_classes = {
+            serializer_classes = {
                'list': MyListSerializer,
                'my_action': MyActionSerializer,
             }
@@ -16,12 +19,12 @@ class MultiSerializerViewSetMixin(object):
             def my_action:
                 ...
 
-        If there's no entry for that action then just fallback to the regular
-        get_serializer_class lookup: self.serializer_class, DefaultSerializer.
-
-        Thanks gonz: http://stackoverflow.com/a/22922156/11440
+        If there's no serializer available for that action than
+        the default serializer class will be returned as fallback.
         """
-        try:
-            return self.serializer_action_classes[self.action]
-        except (KeyError, AttributeError):
-            return super().get_serializer_class()
+        if not isinstance(self.serializer_classes, dict):
+            raise ImproperlyConfigured("serializer_classes should be a dict mapping.")
+
+        if self.action in self.serializer_classes.keys():
+            return self.serializer_classes[self.action]
+        return super().get_serializer_class()
