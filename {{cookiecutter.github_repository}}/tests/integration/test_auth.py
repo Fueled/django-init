429741ad0911ca2d7c5c3702e56a12525d7a304a
Akash Mishra <tucosaurus@gmail.com>
2018-01-22 16:32:05 +0530

    password reset flow first version

--- {{cookiecutter.github_repository}}/tests/integration/test_auth.py
+++ {{cookiecutter.github_repository}}/tests/integration/test_auth.py
@@ -4,7 +4,9 @@ import json
 # Third Party Stuff
 import pytest
 from django.urls import reverse
+from django.contrib.auth.tokens import default_token_generator
 
+from {{cookiecutter.main_module}}.users.services import encode_uid
 from .. import factories as f
 
 pytestmark = pytest.mark.django_db
@@ -46,21 +48,17 @@ def test_user_password_change(client):
     url = reverse('auth-password-change')
     current_password = 'password1'
     new_password = 'paSswOrd2.#$'
-
     user = f.create_user(email='test@example.com')
     user.set_password(current_password)
     user.save()
-
     change_password_payload = {
         'current_password': current_password,
         'new_password': new_password
     }
 
     client.login(user=user)
-
     response = client.json.post(url, json.dumps(change_password_payload))
     assert response.status_code == 204
-
     client.logout()
 
     url = reverse('auth-login')
@@ -68,7 +66,6 @@ def test_user_password_change(client):
         'email': user.email,
         'password': new_password
     }
-
     response = client.json.post(url, json.dumps(credentials))
     assert response.status_code == 200
     expected_keys = [
@@ -76,5 +73,31 @@ def test_user_password_change(client):
     ]
     assert set(expected_keys).issubset(response.data.keys())
 
+    user.refresh_from_db()
+    assert user.check_password(new_password)
+
+
+def test_user_password_reset(client, mailoutbox):
+    url = reverse('auth-password-reset')
+    user = f.create_user(email='test@example.com')
+
+    response = client.json.post(url, json.dumps({'email': user.email}))
+    assert response.status_code == 204
+    assert len(mailoutbox) == 1
+
+
+def test_user_password_reset_confirm(client):
+    url = reverse('auth-password-reset-confirm')
+    user = f.create_user()
+    user.set_password('password1')
+    user.save()
+    new_password = 'paSswOrd2'
+    user_data = {
+        'new_password': new_password,
+        'uid': encode_uid(user.id),
+        'token': default_token_generator.make_token(user)
+    }
+    response = client.json.post(url, json.dumps(user_data))
+    assert response.status_code == 204
     user.refresh_from_db()
     assert user.check_password(new_password)
--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/users/services.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/users/services.py
@@ -1,5 +1,10 @@
 # Third Party Stuff
 from django.contrib.auth import get_user_model, authenticate
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django_sites import get_current
 
 # {{ cookiecutter.project_name }} Stuff
 from {{cookiecutter.main_module}}.base import exceptions as exc
@@ -18,3 +23,45 @@ def create_user_account(email, password, first_name="", last_name=""):
         email=email, password=password, first_name=first_name, last_name=last_name
     )
     return user
+
+
+def encode_uid(pk):
+    return urlsafe_base64_encode(force_bytes(pk)).decode()
+
+
+def decode_uid(pk):
+    return force_text(urlsafe_base64_decode(pk))
+
+
+def send_mail(to_email, from_email, subject, text_body, html_body, context=None):
+
+    subject = render_to_string(subject, context)
+    text_body = render_to_string(text_body, context)
+    html_body = render_to_string(html_body, context)
+
+    # sanitize subject to not contain any new lines
+    subject = ''.join(subject.splitlines())
+    msg = EmailMultiAlternatives(subject=subject, body=text_body,
+                                 from_email=from_email, to=[to_email])
+    msg.attach_alternative(content=html_body, mimetype="text/html")
+    return msg.send()
+
+
+def send_password_reset_mail(user):
+    ctx = {
+        'site': get_current(),
+        'user': user,
+        'url': settings.PASSWORD_RESET_CONFIRM_URL.format(
+            uid=encode_uid(user.id),
+            token=default_token_generator.make_token(user)
+        ),
+        'frontend_domain': settings.FRONTEND_DOMAIN,
+    }
+    to_email = [user.email]
+    from_email = settings.FROM_EMAIL
+    return send_mail(from_email=from_email,
+                     to_email=to_email,
+                     subject='email/password_reset_mail_subject.txt',
+                     text_body='email/password_reset_mail_body.txt',
+                     html_body='email/password_reset_mail_body.html',
+                     context=ctx)
