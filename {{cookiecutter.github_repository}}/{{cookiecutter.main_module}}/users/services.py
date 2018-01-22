62ee8da967ac777f568beab0c7a067b404783b98
Akash Mishra <tucosaurus@gmail.com>
2018-01-22 16:35:12 +0530

    moving from vanilla mail sending to django templated mail

--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/users/services.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/users/services.py
@@ -5,7 +5,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django_sites import get_current
-
+from mail_templated import send_mail
 # {{ cookiecutter.project_name }} Stuff
 from {{cookiecutter.main_module}}.base import exceptions as exc
 
@@ -33,35 +33,16 @@ def decode_uid(pk):
     return force_text(urlsafe_base64_decode(pk))
 
 
-def send_mail(to_email, from_email, subject, text_body, html_body, context=None):
-
-    subject = render_to_string(subject, context)
-    text_body = render_to_string(text_body, context)
-    html_body = render_to_string(html_body, context)
-
-    # sanitize subject to not contain any new lines
-    subject = ''.join(subject.splitlines())
-    msg = EmailMultiAlternatives(subject=subject, body=text_body,
-                                 from_email=from_email, to=[to_email])
-    msg.attach_alternative(content=html_body, mimetype="text/html")
-    return msg.send()
-
-
 def send_password_reset_mail(user):
     ctx = {
         'site': get_current(),
         'user': user,
         'url': settings.PASSWORD_RESET_CONFIRM_URL.format(
-            uid=encode_uid(user.id),
+            uid=encode_uid(user.pk),
             token=default_token_generator.make_token(user)
         ),
         'frontend_domain': settings.FRONTEND_DOMAIN,
     }
-    to_email = [user.email]
+    to_emails = [user.email]
     from_email = settings.FROM_EMAIL
-    return send_mail(from_email=from_email,
-                     to_email=to_email,
-                     subject='email/password_reset_mail_subject.txt',
-                     text_body='email/password_reset_mail_body.txt',
-                     html_body='email/password_reset_mail_body.html',
-                     context=ctx)
+    return send_mail('email/password_reset_mail.tpl', ctx, from_email, to_emails)
