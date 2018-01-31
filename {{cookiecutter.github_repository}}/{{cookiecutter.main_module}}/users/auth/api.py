261383bf52a8ee77e58dc40631a22d020bb24e9f
Akash Mishra <tucosaurus@gmail.com>
2018-01-31 16:20:21 +0530

    Removed email validation from the password reset serializer

--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/users/auth/api.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/users/auth/api.py
@@ -6,7 +6,9 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 # {{ cookiecutter.project_name }} Stuff
 from {{cookiecutter.main_module}}.base import response
 from {{cookiecutter.main_module}}.base.api.mixins import MultipleSerializerMixin
-from {{cookiecutter.main_module}}.users.services import create_user_account, get_and_authenticate_user
+from {{cookiecutter.main_module}}.users.services import (
+    create_user_account, get_and_authenticate_user, get_user_by_email
+)
 
 from . import serializers
 from . import services as auth_services
@@ -51,8 +53,10 @@ class AuthViewSet(MultipleSerializerMixin, viewsets.GenericViewSet):
     def password_reset(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        auth_services.send_password_reset_mail(serializer.user)
-        return response.NoContent()
+        user = get_user_by_email(serializer.data['email'])
+        if user is not None:
+            auth_services.send_password_reset_mail(user)
+        return response.Ok({'message': 'Further instructions will be sent to the email if it exists'})
 
     @list_route(['POST', ])
     def password_reset_confirm(self, request):
