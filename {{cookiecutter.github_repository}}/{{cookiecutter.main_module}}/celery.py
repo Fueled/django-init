ad98f119192fa9cad7502d6f12dfec6b7728e645
Saurabh Kumar <saurabh@saurabh-kumar.com>
2018-03-16 18:01:13 +0530

    chore(celery): minor cleanup

--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/celery.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/celery.py
@@ -8,11 +8,8 @@ import raven
 from celery import Celery
 from django.conf import settings
 from dotenv import load_dotenv
-{%- if cookiecutter.use_sentry_for_error_reporting.lower() == 'y' %}
-from raven.contrib.celery import register_logger_signal, register_signal
-{%- endif %}
 
-# set the default Django settings module for the 'celery' program.
+# Set the default Django settings module for the 'celery' program.
 load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.development')
 {%- if cookiecutter.use_sentry_for_error_reporting.lower() == 'y' %}
@@ -21,13 +18,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.development')
 class CeleryCustomised(Celery):
 
     def on_configure(self):
-        client = raven.Client(os.environ.get('SENTRY_DSN'), environment=os.environ.get('SENTRY_ENVIRONMENT'))
+        client = raven.Client(os.getenv('SENTRY_DSN'), environment=os.getenv('SENTRY_ENVIRONMENT'))
 
         # register a custom filter to filter out duplicate logs
-        register_logger_signal(client)
+        raven.contrib.celery.register_logger_signal(client)
 
         # hook into the Celery error handler
-        register_signal(client)
+        raven.contrib.celery.register_signal(client)
 
 
 app = CeleryCustomised('{{ cookiecutter.main_module }}')
@@ -45,3 +42,5 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.beat_schedule = {}
