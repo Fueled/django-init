febe30151b598d3090593689fc755291bf654d97
Saurabh Kumar <saurabh@saurabh-kumar.com>
2018-04-16 19:54:11 +0530

    Fix tests

--- run_test.sh
+++ run_test.sh
@@ -4,25 +4,27 @@
 err=0
 trap 'err=1' ERR
 
-# Setup
-createdb hello_world
-if [ $? -ne 0 ]; then
+# Clean
+if psql -lqt | cut -d \| -f 1 | grep -qw hello_world ; then
     read -p "Database 'hello_world' required for running the tests already exist. Do you want to delete it (y)?" yn
     if echo "$yn" | grep -iq "^n" ;then
         exit
     else
         dropdb hello_world
-        createdb hello_world
     fi
 fi
+
 rm -rf hello-world-web/;
+
+# Generate new code, (it also creates db, migrate and install dependencies)
 yes 'y' | cookiecutter . --no-input
 
 # Run the tests present inside generate project
 cd hello-world-web;
 ansible-playbook -i provisioner/hosts provisioner/site.yml --syntax-check
-source venv/bin/activate
-flake8
 fab test:"--cov"
+
+# Cleanup
+dropdb hello_world
 
 test $err = 0 # Return non-zero if any command failed
