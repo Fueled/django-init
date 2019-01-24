a9b3094318e9fb3bff7037013be8069f1ac4a8fc
Sanyam Khurana <8039608+CuriousLearner@users.noreply.github.com>
2019-01-24 12:51:56 +0530

    fix(settings/production): Minor spell fix

--- {{cookiecutter.github_repository}}/settings/production.py
+++ {{cookiecutter.github_repository}}/settings/production.py
@@ -69,7 +69,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 #  SECURITY
 # -----------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-# Raises ImproperlyConfigured exception if DJANO_SECRET_KEY not in os.environ
+# Raises ImproperlyConfigured exception if DJANGO_SECRET_KEY not in os.environ
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 if SITE_SCHEME == 'https':
