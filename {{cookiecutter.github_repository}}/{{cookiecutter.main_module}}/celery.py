ebf1fe4b513eca07679a51c39df7bd2af0831a6f
Sanyam Khurana <8039608+CuriousLearner@users.noreply.github.com>
2019-03-23 23:37:42 +0530

    fix(celery.py): Sort import for celery

--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/celery.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/celery.py
@@ -9,7 +9,7 @@ from celery import Celery
 from django.conf import settings
 from dotenv import load_dotenv
 {%- if cookiecutter.use_sentry_for_error_reporting.lower() == 'y'%}
-from raven.contrib.celery import register_signal, register_logger_signal
+from raven.contrib.celery import register_logger_signal, register_signal
 {%- endif %}
 
 # Set the default Django settings module for the 'celery' program.
