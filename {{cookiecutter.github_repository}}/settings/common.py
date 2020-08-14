61b2657d0cf88271f11bbebff303863f9a66f959
Saurabh Kumar <theskumar@users.noreply.github.com>
2020-08-14 18:43:01 +0530

    Add "django.contrib.postgres" to list of INSTALLED_APPS

--- {{cookiecutter.github_repository}}/settings/common.py
+++ {{cookiecutter.github_repository}}/settings/common.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = (
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "django.contrib.postgres",
     "django_sites",  # http://niwinz.github.io/django-sites/latest/
     "django.contrib.messages",
     "django.contrib.staticfiles",
