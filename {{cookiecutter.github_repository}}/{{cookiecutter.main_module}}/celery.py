e9ff327fca2d182ed61c9f80312ebf7d4f22e851
Saurabh Kumar <theskumar@users.noreply.github.com>
2020-09-29 19:49:34 +0530

    Add more stuff affected by black

--- {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/celery.py
+++ {{cookiecutter.github_repository}}/{{cookiecutter.main_module}}/celery.py
@@ -13,15 +13,17 @@ from raven.contrib.celery import register_logger_signal, register_signal
 {%- endif %}
 
 # Set the default Django settings module for the 'celery' program.
-load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.development')
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
 {%- if cookiecutter.use_sentry_for_error_reporting.lower() == 'y' %}
 
 
 class CeleryCustomised(Celery):
 
     def on_configure(self):
-        client = raven.Client(os.getenv('SENTRY_DSN'), environment=os.getenv('SENTRY_ENVIRONMENT'))
+        client = raven.Client(
+            os.getenv("SENTRY_DSN"), environment=os.getenv("SENTRY_ENVIRONMENT")
+        )
 
         # Always ensure you import register_logger_signal, register_signal and not their parent modules
         # register a custom filter to filter out duplicate logs
@@ -31,18 +33,18 @@ class CeleryCustomised(Celery):
         register_signal(client)
 
 
-app = CeleryCustomised('{{ cookiecutter.main_module }}')
+app = CeleryCustomised("{{ cookiecutter.main_module }}")
 {%- else %}
 
 
-app = Celery('{{ cookiecutter.main_module }}')
+app = Celery("{{ cookiecutter.main_module }}")
 {%- endif %}
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
