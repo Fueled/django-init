a1722c746057fa8a91c6f69aaebab8f79675c1ac
suneet <suneet@fueled.com>
2022-12-20 18:19:17 +0530

    chore(graphql): add test cases

--- {{cookiecutter.github_repository}}/tests/graphql/test_auth.py
+++ {{cookiecutter.github_repository}}/tests/graphql/test_auth.py
@@ -98,3 +98,95 @@ def test_user_login(client):
     assert "errors" not in response_data.keys()
     assert set(expected_keys).issubset(response_data["data"]["login"]["user"].keys())
 
+
+def test_user_login_with_incorrect_creds(client):
+    graphql_query = """
+                    mutation Login {
+                        login (
+                            input: {
+                                email: "test@example.com",
+                                password: "incorrect_password"
+                            }
+                        ) {
+                            user {
+                                email
+                                firstName
+                                lastName
+                                authToken
+                            }
+                        }
+                    }
+                    """
+    f.create_user(email="test@example.com", password="password")
+
+    response = client.post_graphql(graphql_query)
+    assert response.status_code == 200
+
+    # should return token in response
+    response_data = json.loads(response.content)
+    assert "errors" in response_data.keys()
+    assert (
+        "Invalid username/password. Please try again!"
+        == response_data["errors"][0]["message"]
+    )
+
+
+def test_user_password_change(client):
+    graphql_query = """
+                    mutation PasswordChange {
+                        passwordChange (
+                            input: {
+                                currentPassword: "pass123word", newPassword:"new123password"
+                            }
+                        ) {
+                            user {
+                                email
+                                firstName
+                                lastName
+                                authToken
+                            }
+                        }
+                    }
+                    """
+    user = f.create_user(email="test@example.com", password="pass123word")
+
+    response = client.post_graphql(graphql_query)
+    response_data = json.loads(response.content)
+    assert "errors" in response_data.keys()
+
+    client.login(user)
+
+    response = client.post_graphql(graphql_query)
+    assert response.status_code == 200
+
+    # should return token in response
+    response_data = json.loads(response.content)
+    assert "errors" not in response_data.keys()
+    expected_keys = ["authToken", "email", "firstName", "lastName"]
+    assert set(expected_keys).issubset(
+        response_data["data"]["passwordChange"]["user"].keys()
+    )
+
+
+def test_user_request_password_reset(client):
+    graphql_query = """
+                    mutation RequestPasswordReset {
+                        passwordReset (
+                            input: {
+                                email: "test@example.com"
+                            }
+                        ) {
+                            message
+                        }
+                    }
+                    """
+    user = f.create_user(email="test@example.com", password="pass123word")
+
+    response = client.post_graphql(graphql_query)
+    response_data = json.loads(response.content)
+    assert "errors" not in response_data.keys()
+    expected_keys = ["message", "email", "firstName", "lastName"]
+    assert (
+        "Further instructions will be sent to the email if it exists"
+        == response_data["data"]["passwordReset"]["message"]
+    )
