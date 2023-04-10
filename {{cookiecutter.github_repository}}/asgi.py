6dbcd68e8f0e07fefea4ea826c21ee622c86a59c
suneet <suneet@fueled.com>
2023-04-10 15:23:56 +0530

    chore(async): add cookiecutter option to opt for asgi or use wsgi

--- {{cookiecutter.github_repository}}/asgi.py
+++ {{cookiecutter.github_repository}}/asgi.py
@@ -5,13 +5,11 @@ import os
 from django.core.asgi import get_asgi_application
 from dotenv import load_dotenv
 
-# Read .env file and set key/value inside it as environement variables
+# Read .env file and set key/value inside it as environment variables
 # see: http://github.com/theskumar/python-dotenv
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
+# We defer to a DJANGO_SETTINGS_MODULE already in the environment.
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
 
