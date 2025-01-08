b8cfd2e8973df4ec70f408c93fc18b6e5976ab2d
kunal kumar kushwaha <kunalkumar.kus@gmail.com>
2025-01-08 16:11:06 +0530

    Update production.py for redis issue (#477)

--- {{cookiecutter.github_repository}}/settings/production.py
+++ {{cookiecutter.github_repository}}/settings/production.py
@@ -145,7 +145,7 @@ CACHES = {
         "LOCATION": env("REDIS_URL", default="redis://localhost:6379/0"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "PARSER_CLASS": "redis.connection.HiredisParser",
+            "PARSER_CLASS": "redis.connection._HiredisParser",
             "CONNECTION_POOL_CLASS": "redis.BlockingConnectionPool",
             "CONNECTION_POOL_CLASS_KWARGS": {
 {%- if cookiecutter.add_heroku.lower() == "y" %}
